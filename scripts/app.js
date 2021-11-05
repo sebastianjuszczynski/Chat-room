21e98d993470f9376ce7ea853915e73a0698e1ad
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-11-05 16:18:15 +0000

    functionality for updating a room created

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMessage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -23,6 +24,14 @@ newNameForm.addEventListener("submit", e => {
     }, 3000)
     
     
+});
+
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
 })
 
 const username = localStorage.username ? localStorage.username : "unknown"
