99a582721a5764ad17c28449d5c28c349d290ce9
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-11-05 14:00:33 +0000

    method addChat created

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,6 +4,15 @@ class Chatroom {
         this.username = username;
         this.chats = db.collection('chats');
     }
+    async addChat(message){
+        const now = new Date();
+        const chat = {
+            message,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(now) 
+        };
+    }
 }
 
 const chatroom = new Chatroom('sport', 'seba');
