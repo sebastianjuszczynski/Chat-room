9c6725999516d2f42b923f0569c2e82a4c110e3c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-11-05 15:37:04 +0000

    setup listener for sending a new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim(); 
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+})
 
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'seba');
