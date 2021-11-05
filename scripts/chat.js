a84b4d1a679bcfe477434a4ada3b9d8f35eaea5e
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-11-05 14:54:29 +0000

    created methods updateName and updateRoom. added unsubscribe as well

--- scripts/chat.js
+++ scripts/chat.js
@@ -3,34 +3,54 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
-    async addChat(message){
+    async addChat(message) {
         const now = new Date();
         const chat = {
             message,
             username: this.username,
             room: this.room,
-            created_at: firebase.firestore.Timestamp.fromDate(now) 
+            created_at: firebase.firestore.Timestamp.fromDate(now)
         };
         const response = await this.chats.add(chat);
         return response;
     }
-    getChats(callback){
-        this.chats
+    getChats(callback) {
+        this.unsub = this.chats
             .where('room', '==', this.room)
             .orderBy('created_at')
             .onSnapshot(snapshot => {
                 snapshot.docChanges().forEach(change => {
-                    if(change.type === 'added'){
+                    if (change.type === 'added') {
                         callback(change.doc.data())
                     }
                 })
             })
     }
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub();
+        }
+    }
 }
 
-const chatroom = new Chatroom('sport', 'seba');
+const chatroom = new Chatroom('general', 'seba');
 
 chatroom.getChats((data) => {
     console.log(data)
-})
+});
+
+setTimeout(() => {
+    chatroom.updateRoom('music');
+    chatroom.updateName('Amelka')
+    chatroom.getChats((data) => {
+        console.log(data)
+    });
+    chatroom.addChat('hellooooo')
+}, 2000)
